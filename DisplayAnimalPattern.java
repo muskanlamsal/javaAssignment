dd9d97b364910a462b7ea19915274abe6dde5530
muskanlamsal <sportymskn@gmail.com>
2015-02-10 00:38:30 +0545

    Familiar with symbols.

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,17 @@
+package com.muskan.TestWork;
+
+/**
+ * Created by muskan on 1/21/15.
+ */
+public class DisplayAnimalPattern {
+    public static void main(String[] args) {
+        System.out.println("            '__'");
+        System.out.println("            (\u00A9\u00A9)");
+        System.out.println("    =========\\/");
+        System.out.println("  / || %% ||");
+        System.out.println(" *  ||----||");
+        System.out.println("    \u00A5\u00A5    \u00A5\u00A5");
+        System.out.println("    \"\"    \"\"");
+
+    }
+}
